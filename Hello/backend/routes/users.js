2063c34010bdae2c649255c4f7be0d942ded3d03
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2019-12-10 23:00:06 +0330

    fix minor bugs

--- Hello/backend/routes/users.js
+++ Hello/backend/routes/users.js
@@ -255,7 +255,7 @@ router.post("/contact-form", async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
     req.body.ip = req.connection.remoteAddress;
     logger.info(req.body);
-    res.send("OK");
+    res.send({msg:"OK"});
 });
 
 module.exports = router;
