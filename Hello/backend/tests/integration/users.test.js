41748e3fff24700cb3e5ac5cfcce1d20f9022df3
HamidReza Nasrollahi <nasrollahy.hamidreza@gmail.com>
2019-12-16 17:01:29 +0330

    add test to project and wanna add another route with tdd

--- Hello/backend/tests/integration/users.test.js
+++ Hello/backend/tests/integration/users.test.js
@@ -1,16 +1,17 @@
 const request = require("supertest");
 const bcrypt = require("bcryptjs");
 const { User } = require("../../models/user");
+// const expect = require("chai").expect;
 
 describe("/api/users/test", () => {
     let server;
-    server = require("../../index");
 
     // beforeAll(() => {
+    //     server = require("../../index");
     // });
-    // beforeEach(() => {
-    //     // server = require("../../index");
-    // });
+    beforeEach(() => {
+        server = require("../../index");
+    });
     // afterAll(async () => {
     //     try {
     //         // await User.remove({});
@@ -20,143 +21,147 @@ describe("/api/users/test", () => {
     //         console.log(error);
     //     }
     // });
-    // afterEach(async () => {
-    //     try {
-    //         await User.remove({});
-    //         await server.close();
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
+    afterEach(async () => {
+        try {
+            await User.remove({});
+            await server.close();
+        } catch (error) {
+            console.log(error);
+        }
+    });
+    // describe("Array", function () {
+    //     describe("#indexOf()", function () {
+    //         // it("should return -1 when the value is not present", function () {
+    //         //     assert.equal([1, 2, 3].indexOf(4), 0);
+    //         // });
+    //         it("should return 200 after login", async () => {
+    //             // var mg = require("mongoose");
+
+    //             // mg.connect('mongodb://localhost/cat_test');
+
+    //             // var Cat = mg.model('Cat', { name: String });
+    //             // var kitty = new Cat({ name: 'Zildjian' });
+    //             // console.log("1");
+    //             const name = "testtest3";
+    //             const email = "test@test.com";
+    //             const password = "123456";
+    //             const user = new User({
+    //                 "name": name,
+    //                 "email": email,
+    //                 "password": password
+    //             });
+    //             // request(server).post("/hello/api/users/login").send({ "email": email, "password": password }).expect(400).end(async function (err, res) {
+    //             //     if (err) throw err;
+    //             //     await User.remove({});
+    //             // });
+    //             // done();
+
+    //             const salt = bcrypt.genSaltSync(10);
+    //             const hash = bcrypt.hashSync(password, salt);
+    //             user.password = hash;
+    //             await user.save();
+    //             const result = await request(server).post("/hello/api/users/login").send({ "email": email, "password": password });
+    //             await User.remove({});
+    //             expect(result.status).to.equal(200);
+    //             // try {
+    //             //     console.log("sssss");
+    //             //     request(server).post("/hello/api/users/login").send({ "email": email, "password": password }).expect(10).end(async function (err, res) {
+    //             //         if (err) throw err;
+    //             //         console.log(res);
+    //             //     });
+    //             //     // await User.remove({});
+
+    //             // } catch (error) {
+    //             //     expect(error).toMatch("error");
+    //             // }
+
+    //         });
+    //     });
     // });
-    var assert = require('assert');
-    describe('Array', function () {
-        describe('#indexOf()', function () {
-            // it('should return -1 when the value is not present', function () {
-            //     assert.equal([1, 2, 3].indexOf(4), 0);
-            // });
-            it("should return 200 after login", async () => {
-                // var mg = require('mongoose');
-
-                // mg.connect('mongodb://localhost/cat_test');
-
-                // var Cat = mg.model('Cat', { name: String });
-                // var kitty = new Cat({ name: 'Zildjian' });
-                // console.log("1");
-                const name = "testtest3";
-                const email = "test@test.com";
+    describe("POST /hello/api/users/list ", () => {
+        /* ------------------------------- happy path ------------------------------- */
+        it("should return count of three document of users collection ", async () => {
+            for (let index = 0; index < 3; index++) {
+                const name = "testtest"+index;
+                const email = "test@test.com"+index;
                 const password = "123456";
                 const user = new User({
                     "name": name,
                     "email": email,
                     "password": password
                 });
+
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(password, salt);
+                user.password = hash;
                 await user.save();
-                // request(server).post("/hello/api/users/login").send({ "email": email, "password": password }).expect(400).end(async function (err, res) {
-                //     if (err) throw err;
-                //     await User.remove({});
-                // });
-                const result = await request(server).post("/hello/api/users/login");
-                await User.remove({});
-                // done();
 
-                // bcrypt.genSalt(10, async function (err, salt) {
-                //     bcrypt.hash(password, salt, async function (err, hash) {
-                //         user.password = hash;
-                //         try {
-                //             console.log("sssss");
-                //             request(server).post("/hello/api/users/login").send({ "email": email, "password": password }).expect(10).end(async function (err, res) {
-                //                 if (err) throw err;
-                //                 console.log(res);
-                //             });
-                //             // await User.remove({});
+            }
+            const result = await request(server).get("/hello/api/users/list").header({  });
+            expect(result.status).toBe(200);
+            
+        });
+        it("should return 400 status code if you don't have permission for have users list ", () => {
 
-                //         } catch (error) {
-                //             expect(error).toMatch("error");
-                //         }
-                //     });
-                // });
-            });
         });
     });
-    // describe("POST /hello/api/users/login ", () => {
-    // it("should return 200 after login", async () => {
-    //     console.log("1");
-    //     const name = "testtest3";
-    //     const email = "test@test.com";
-    //     const password = "123456";
-    //     const user = new User({
-    //         "name": name,
-    //         "email": email,
-    //         "password": password
-    //     });
+    describe("POST /hello/api/users/login ", () => {
+        it("should return 200 after login", async () => {
+            const name = "testtest3";
+            const email = "test@test.com";
+            const password = "123456";
+            const user = new User({
+                "name": name,
+                "email": email,
+                "password": password
+            });
 
-    //     bcrypt.genSalt(10, async function (err, salt) {
-    //         bcrypt.hash(password, salt, async function (err, hash) {
-    //             user.password = hash;
-    //             try {
-    //                 await user.save();
-    //                 console.log("sssss");
-    //                 request(server).post("/hello/api/users/login").send({ "email": email, "password": password }).expect(10).end(async function (err, res) {
-    //                     if (err) throw err;
-    //                     console.log(res);
-    //                 });
-    //                 // await User.remove({});
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+            user.password = hash;
+            await user.save();
+            const result = await request(server).post("/hello/api/users/login").send({ "email": email, "password": password });
+            expect(result.status).toBe(200);
+            // await User.remove({});
 
-    //             } catch (error) {
-    //                 expect(error).toMatch("error");
-    //             }
-    //         });
-    //     });
-    //     // });
-    //     // it("should return 400 if email was wrong", async () => {
-    //     //     console.log("2");
-    //     //     const name = "testtest2";
-    //     //     const email = "test@test.com";
-    //     //     const password = "123456";
-    //     //     const user = new User({
-    //     //         "name": name,
-    //     //         "email": email,
-    //     //         "password": password
-    //     //     });
+        });
+        it("should return 400 if email was wrong", async () => {
+            const name = "testtest2";
+            const email = "test@test.com";
+            const password = "123456";
+            const user = new User({
+                "name": name,
+                "email": email,
+                "password": password
+            });
 
-    //     //     bcrypt.genSalt(10, async function (err, salt) {
-    //     //         bcrypt.hash(password, salt, async function (err, hash) {
-    //     //             user.password = hash;
-    //     //             try {
-    //     //                 await user.save();
-    //     //                 const response = await request(server).post("/hello/api/users/login").send({ "email": "wrongEmail", "password": password });
-    //     //                 await User.remove({});
-    //     //                 expect(response.statusCode).toBe(400);
-    //     //             } catch (error) {
-    //     //                 expect(error).toMatch("error");
-    //     //             }
-    //     //         });
-    //     //     });
-    //     // });
-    //     // it("should return 400 if password was wrong", async () => {
-    //     //     console.log("3");
-    //     //     const name = "testtest1";
-    //     //     const email = "test@test.com";
-    //     //     const password = "123456";
-    //     //     const user = new User({
-    //     //         "name": name,
-    //     //         "email": email,
-    //     //         "password": password
-    //     //     });
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+            user.password = hash;
+            await user.save();
+            const result = await request(server).post("/hello/api/users/login").send({ "email": "wrongEmail", "password": password });
+            await User.remove({});
+            expect(result.status).toBe(400);
+        });
+        it("should return 400 if password was wrong", async () => {
+            console.log("3");
+            const name = "testtest1";
+            const email = "test@test.com";
+            const password = "123456";
+            const user = new User({
+                "name": name,
+                "email": email,
+                "password": password
+            });
 
-    //     //     bcrypt.genSalt(10, async function (err, salt) {
-    //     //         bcrypt.hash(password, salt, async function (err, hash) {
-    //     //             user.password = hash;
-    //     //             try {
-    //     //                 await user.save();
-    //     //                 const response = await request(server).post("/hello/api/users/login").send({ "email": email, "password": "WrongPass" });
-    //     //                 await User.remove();
-    //     //                 expect(response.statusCode).toBe(400);
-    //     //             } catch (error) {
-    //     //                 expect(error).toMatch("error");
-    //     //             }
-    //     //         });
-    //     //     });
-    //     // });
-    // });
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+            user.password = hash;
+            await user.save();
+            const result = await request(server).post("/hello/api/users/login").send({ "email": email, "password": "WrongPass" });
+            await User.remove();
+            expect(result.status).toBe(400);
+
+        });
+    });
 });
