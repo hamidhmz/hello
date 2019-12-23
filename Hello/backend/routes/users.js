10d07846f587feea54208dba044a33b04a5150fc
HamidReza Nasrollahi <nasrollahy.hamidreza@gmail.com>
2019-12-23 12:00:33 +0330

    fix minor bugs

--- Hello/backend/routes/users.js
+++ Hello/backend/routes/users.js
@@ -259,7 +259,6 @@ router.post("/contact-form", async (req, res) => {
     req.body.ip2 = req.headers["x-forwarded-for"];
     // req.body.message2 = req.body.message;
     
-    console.log(JSON.parse(req.body));
     try {
         await ContactUs.create(
             req.body
