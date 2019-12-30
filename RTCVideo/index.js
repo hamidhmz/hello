221ce152e23a7c57bd070f7151bb870b18cd01cb
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2019-12-30 23:46:30 +0330

    add video call to project

--- RTCVideo/index.js
+++ RTCVideo/index.js
@@ -1,76 +1,119 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-const server = require("http").Server(app);
-let io = require('socket.io')(server);
-app.set("view engine", "ejs");
-app.set("views", "./");
-app.use(express.static(__dirname ));
-function connection(socket) {
-	var channel_id;
-
-	function disconnect() {
-		console.log("disconnected");
-
-		if (channel_id && channels[channel_id]) {
-			for (var i=0; i<channels[channel_id].sockets.length; i++) {
-				if (socket !== channels[channel_id].sockets[i]) {
-					channels[channel_id].sockets[i].emit("disconnect");
-				}
-				channels[channel_id].sockets[i].leave("channel:" + channel_id);
-				channels[channel_id].sockets[i] = null;
-			}
-			channels[channel_id] = null;
-			channel_id = null;
-		}
-	}
-
-	function onSignal(msg) {
-		console.log("relaying signal:",msg);
-		socket.broadcast.emit("signal",msg);
-	}
-
-	socket.on("disconnect",disconnect);
-	socket.on("signal",onSignal);
-
-	// is there a channel waiting for a socket to join it?
-	if (
-		channels.length > 0 &&
-		channels[channels.length-1] &&
-		channels[channels.length-1].sockets.length === 1
-	) {
-		console.log("sockets joining channel: " + (channels.length-1));
-
-		channels[channels.length-1].sockets.push(socket);
-
-		// join both sockets to the channel
-		for (var i=0; i<2; i++) {
-			channels[channels.length-1].sockets[i].join("channel:" + channel_id);
-		}
-
-		// identify caller and receiver
-		channels[channels.length-1].sockets[0].emit("identify",/*caller=*/true);
-		channels[channels.length-1].sockets[1].emit("identify",/*caller=*/false);
-	}
-  
-	// make a new channel
-	else {
-		channels[channels.length] = {
-			sockets: [socket]
-		};
-	}
-
-	// save this socket's channel_id
-	channel_id = channels.length - 1;
-
-	console.log("user assigned to channel: " + channel_id);
-}
+/**************/
+/*** CONFIG ***/
+/**************/
+var PORT = 8080;
 
 
-// app.get('/', (req, res) => {
-// 	res.render("11", { page: "video-call" });
-// });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+/*************/
+/*** SETUP ***/
+/*************/
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser')
+var app = express()
+var server = http.createServer(app)
+var io  = require('socket.io').listen(server);
+//io.set('log level', 2);
 
-io.of("/rtc").on("connection",connection);
+server.listen(PORT, null, function() {
+    console.log("Listening on port " + PORT);
+});
+//app.use(express.bodyParser());
+
+app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html'); });
+// app.get('/index.html', function(req, res){ res.sendfile('newclient.html'); });
+// app.get('/client.html', function(req, res){ res.sendfile('newclient.html'); });
+
+
+
+/*************************/
+/*** INTERESTING STUFF ***/
+/*************************/
+var channels = {};
+var sockets = {};
+
+/**
+ * Users will connect to the signaling server, after which they'll issue a "join"
+ * to join a particular channel. The signaling server keeps track of all sockets
+ * who are in a channel, and on join will send out 'addPeer' events to each pair
+ * of users in a channel. When clients receive the 'addPeer' even they'll begin
+ * setting up an RTCPeerConnection with one another. During this process they'll
+ * need to relay ICECandidate information to one another, as well as SessionDescription
+ * information. After all of that happens, they'll finally be able to complete
+ * the peer connection and will be streaming audio/video between eachother.
+ */
+io.sockets.on('connection', function (socket) {
+    socket.channels = {};
+    sockets[socket.id] = socket;
+
+    console.log("["+ socket.id + "] connection accepted");
+    socket.on('disconnect', function () {
+        for (var channel in socket.channels) {
+            part(channel);
+        }
+        console.log("["+ socket.id + "] disconnected");
+        delete sockets[socket.id];
+    });
+
+
+    socket.on('join', function (config) {
+        console.log("["+ socket.id + "] join ", config);
+        var channel = config.channel;
+        var userdata = config.userdata;
+
+        if (channel in socket.channels) {
+            console.log("["+ socket.id + "] ERROR: already joined ", channel);
+            return;
+        }
+
+        if (!(channel in channels)) {
+            channels[channel] = {};
+        }
+
+        for (id in channels[channel]) {
+            channels[channel][id].emit('addPeer', {'peer_id': socket.id, 'should_create_offer': false});
+            socket.emit('addPeer', {'peer_id': id, 'should_create_offer': true});
+        }
+
+        channels[channel][socket.id] = socket;
+        socket.channels[channel] = channel;
+    });
+
+    function part(channel) {
+        console.log("["+ socket.id + "] part ");
+
+        if (!(channel in socket.channels)) {
+            console.log("["+ socket.id + "] ERROR: not in ", channel);
+            return;
+        }
+
+        delete socket.channels[channel];
+        delete channels[channel][socket.id];
+
+        for (id in channels[channel]) {
+            channels[channel][id].emit('removePeer', {'peer_id': socket.id});
+            socket.emit('removePeer', {'peer_id': id});
+        }
+    }
+    socket.on('part', part);
+
+    socket.on('relayICECandidate', function(config) {
+        var peer_id = config.peer_id;
+        var ice_candidate = config.ice_candidate;
+        console.log("["+ socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
+
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('iceCandidate', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
+        }
+    });
+
+    socket.on('relaySessionDescription', function(config) {
+        var peer_id = config.peer_id;
+        var session_description = config.session_description;
+        console.log("["+ socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
+
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('sessionDescription', {'peer_id': socket.id, 'session_description': session_description});
+        }
+    });
+});
