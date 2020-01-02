3f27dd6221054ccfb5f5f3d22cde6ffc17925a0d
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2020-01-02 16:51:37 +0330

    fix minor bugs  and change nginx config

--- RTCVideo/index.js
+++ RTCVideo/index.js
@@ -12,15 +12,18 @@ var http = require('http');
 var bodyParser = require('body-parser')
 var app = express()
 var server = http.createServer(app)
-var io  = require('socket.io').listen(server);
+var io = require('socket.io')(server, {
+    path: "/video-call"
+});
+// var io  = require('socket.io').listen(server);
 //io.set('log level', 2);
 
-server.listen(PORT, null, function() {
+server.listen(PORT, null, function () {
     console.log("Listening on port " + PORT);
 });
 //app.use(express.bodyParser());
 
-app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html'); });
+app.get('/', function (req, res) { res.sendFile(__dirname + '/index.html'); });
 // app.get('/index.html', function(req, res){ res.sendfile('newclient.html'); });
 // app.get('/client.html', function(req, res){ res.sendfile('newclient.html'); });
 
@@ -46,23 +49,23 @@ io.sockets.on('connection', function (socket) {
     socket.channels = {};
     sockets[socket.id] = socket;
 
-    console.log("["+ socket.id + "] connection accepted");
+    console.log("[" + socket.id + "] connection accepted");
     socket.on('disconnect', function () {
         for (var channel in socket.channels) {
             part(channel);
         }
-        console.log("["+ socket.id + "] disconnected");
+        console.log("[" + socket.id + "] disconnected");
         delete sockets[socket.id];
     });
 
 
     socket.on('join', function (config) {
-        console.log("["+ socket.id + "] join ", config);
+        console.log("[" + socket.id + "] join ", config);
         var channel = config.channel;
         var userdata = config.userdata;
 
         if (channel in socket.channels) {
-            console.log("["+ socket.id + "] ERROR: already joined ", channel);
+            console.log("[" + socket.id + "] ERROR: already joined ", channel);
             return;
         }
 
@@ -71,8 +74,8 @@ io.sockets.on('connection', function (socket) {
         }
 
         for (id in channels[channel]) {
-            channels[channel][id].emit('addPeer', {'peer_id': socket.id, 'should_create_offer': false});
-            socket.emit('addPeer', {'peer_id': id, 'should_create_offer': true});
+            channels[channel][id].emit('addPeer', { 'peer_id': socket.id, 'should_create_offer': false });
+            socket.emit('addPeer', { 'peer_id': id, 'should_create_offer': true });
         }
 
         channels[channel][socket.id] = socket;
@@ -80,10 +83,10 @@ io.sockets.on('connection', function (socket) {
     });
 
     function part(channel) {
-        console.log("["+ socket.id + "] part ");
+        console.log("[" + socket.id + "] part ");
 
         if (!(channel in socket.channels)) {
-            console.log("["+ socket.id + "] ERROR: not in ", channel);
+            console.log("[" + socket.id + "] ERROR: not in ", channel);
             return;
         }
 
@@ -91,29 +94,29 @@ io.sockets.on('connection', function (socket) {
         delete channels[channel][socket.id];
 
         for (id in channels[channel]) {
-            channels[channel][id].emit('removePeer', {'peer_id': socket.id});
-            socket.emit('removePeer', {'peer_id': id});
+            channels[channel][id].emit('removePeer', { 'peer_id': socket.id });
+            socket.emit('removePeer', { 'peer_id': id });
         }
     }
     socket.on('part', part);
 
-    socket.on('relayICECandidate', function(config) {
+    socket.on('relayICECandidate', function (config) {
         var peer_id = config.peer_id;
         var ice_candidate = config.ice_candidate;
-        console.log("["+ socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
+        console.log("[" + socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
 
         if (peer_id in sockets) {
-            sockets[peer_id].emit('iceCandidate', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
+            sockets[peer_id].emit('iceCandidate', { 'peer_id': socket.id, 'ice_candidate': ice_candidate });
         }
     });
 
-    socket.on('relaySessionDescription', function(config) {
+    socket.on('relaySessionDescription', function (config) {
         var peer_id = config.peer_id;
         var session_description = config.session_description;
-        console.log("["+ socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
+        console.log("[" + socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
 
         if (peer_id in sockets) {
-            sockets[peer_id].emit('sessionDescription', {'peer_id': socket.id, 'session_description': session_description});
+            sockets[peer_id].emit('sessionDescription', { 'peer_id': socket.id, 'session_description': session_description });
         }
     });
 });
