8309faf9c49bbb42a4b4a1e59ccd1ad3fec00007
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2020-02-12 01:41:34 +0330

    change qotation

--- Hello/backend/startup/validation.js
+++ Hello/backend/startup/validation.js
@@ -1,5 +1,5 @@
-const Joi = require("joi");
-const joi_objectid = require("joi-objectid");
+const Joi = require('joi');
+const joi_objectid = require('joi-objectid');
 
 module.exports = function(){
     Joi.ObjectId = joi_objectid(Joi);
