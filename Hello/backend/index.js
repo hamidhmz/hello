6b548898df175df30b58dc6c4c887f5e55364405
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2020-02-28 22:26:06 +0330

    change is don

--- Hello/backend/index.js
+++ Hello/backend/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 'use strict';
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,6 +8,7 @@ const startupConfig = require('./startup/config');
 const startupValidation = require('./startup/validation');
 const startupProd = require('./startup/prod');
 const startupSocket = require('./startup/socket');
+const startupCache = require('./startup/cache');
 const cookieParser = require('cookie-parser');
 const config = require('config');
 
@@ -20,19 +20,15 @@ app.set('views', '../frontend');
 app.use('/hello', express.static(__dirname + '/../frontend/'));
 app.use(cookieParser());
 app.set('trust proxy');
-// app.use(express.json());
 
 startupRoutes(app);
 startupDb(mongoose);
 startupConfig();
 startupValidation();
 startupProd(app);
+startupCache;
 const server = startupSocket(app);
 
-// const port = process.env.PORT || 3000;
-app.get('/2222', (req, res) => {
-    res.send(200);
-});
 const server1 = server.listen(config.get('PORT'), () =>
     logger.info(`Listening on port ${config.get('PORT')}...`)
 );
